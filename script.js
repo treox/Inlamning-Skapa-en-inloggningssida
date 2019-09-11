ab62f86722568c7a16d3b865dcc221a1ae8a3da7
Tero Viitala <vite19ax@student.ju.se>
2019-09-11 15:53:15 +0200

    Skapat js fil Issue: Skapa en js fil och kod med åtkomst till element

---
+++ script.js
@@ -0,0 +1,5 @@
+
+let bodyE = document.getElementById("body-id");
+let inpNamn = document.getElementById("namn-id");
+let inpLosen = document.getElementById("losen-id");
+let loginB = document.getElementById("loginb-id");
